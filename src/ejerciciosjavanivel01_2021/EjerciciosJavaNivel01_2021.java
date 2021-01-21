f51e09ddc999d45753c2ff9ba410cbe8aed94b9d
xp <xp@CETYSDAM00>
2021-01-21 13:11:38 +0100

    Ejercicios Nivel 01 de Java
    primera version de mi ejercicio 01

--- src/ejerciciosjavanivel01_2021/EjerciciosJavaNivel01_2021.java
+++ src/ejerciciosjavanivel01_2021/EjerciciosJavaNivel01_2021.java
@@ -13,10 +13,16 @@ public class EjerciciosJavaNivel01_2021 {
 
     //declaro un array de integers para hacer pruebas
     
-    int[] listaNumeros = {333, 31, 200, 20, 5, 99};
+    int[] listaNumeros = {3, 31, 200, 20, 5, 99};
     
     public int maximo (int[] lista){
-        
+        int auxiliar = lista[0]; //el maximo va a ser el primer elemento del array
+        for (int i=0; i< lista.length; i++){
+            if (auxiliar < lista[i]){
+            auxiliar = lista [i];
+            }
+         }
+        return auxiliar;
     }
 
     /**
@@ -26,6 +32,7 @@ public class EjerciciosJavaNivel01_2021 {
         EjerciciosJavaNivel01_2021 ejercicio = new EjerciciosJavaNivel01_2021();
         
         System.out.println(ejercicio.listaNumeros[0]);
+        System.out.println("El maximo es " + ejercicio.maximo(ejercicio.listaNumeros));
         
     }
     
