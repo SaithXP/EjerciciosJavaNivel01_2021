f2eed6b0f681c6f3d0687d4203c1208d9997480b
xp <xp@CETYSDAM00>
2021-02-04 13:17:32 +0100

    ejercicios de nivel 1 de java ekisde
    con la prueba del isograma

--- src/ejerciciosjavanivel01_2021/EjerciciosJavaNivel01_2021.java
+++ src/ejerciciosjavanivel01_2021/EjerciciosJavaNivel01_2021.java
@@ -24,8 +24,29 @@ public class EjerciciosJavaNivel01_2021 {
          }
         return auxiliar;
     }
-
-    /**
+    
+        public boolean esIsograma( String palabra){
+        //este metodo devuelve true si una palabra no tiene
+        //ninguna letra repetida
+        //y false si se repite alguna
+        //MURCIELAGO
+        for (int i=0; i < palabra.length(); i++){
+            for(int j=i+1; j < palabra.length(); j++){
+                if(palabra.charAt(i) == palabra.charAt(j)){
+                    return false;
+                }
+                
+            }
+        }
+        //si ah recorrido los dos bucles sin salir del return false,
+        //es porque la palabra no tiene letras repetidas
+        
+        return true;
+    }
+    
+        
+    
+    /*
      * @param args 
      */
     public static void main(String[] args) {
@@ -34,6 +55,10 @@ public class EjerciciosJavaNivel01_2021 {
         System.out.println(ejercicio.listaNumeros[0]);
         System.out.println("El maximo es " + ejercicio.maximo(ejercicio.listaNumeros));
         
+        System.out.println("la palabra Murcielago: " + ejercicio.esIsograma("murcielago"));
+        System.out.println("la palabra careta: " + ejercicio.esIsograma("careta"));
+        
     }
-    
 }
+    
+
